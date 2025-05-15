e640a0dc8666a819376e9b7424cb263fe5e72321
gulas322 <2289340005@qq.com>
2025-05-15 20:04:22 +0800

    Mapper 实现sql单独书写，而不是注解，可以书写复杂sql

--- src/main/java/com/itheima/dao/BookDao.java
+++ src/main/java/com/itheima/dao/BookDao.java
@@ -11,18 +11,18 @@ public interface BookDao {
 
     // 这里的添加、删除、修改、查询方法都简单来写，只写与数据库相关的操作，不要搞复杂了返回值
     // 复杂的内容放到service层来做
-    @Insert("insert into tbl_book values(null,#{type},#{name},#{description})")
+//    @Insert("insert into tbl_book values(null,#{type},#{name},#{description})")
     void addBook(Book book);
 
-    @Delete("delete from tbl_book where id = #{id}")
+//    @Delete("delete from tbl_book where id = #{id}")
     void deleteBookById(Integer id);
 
-    @Update("update tbl_book set type = #{type}, name = #{name}, description = #{description} where id = #{id}")
+//    @Update("update tbl_book set type = #{type}, name = #{name}, description = #{description} where id = #{id}")
     void updateBook(Book book);
 
-    @Select("select * from tbl_book where id = #{id}")
+//    @Select("select * from tbl_book where id = #{id}")
     Book queryBookById(Integer id);
 
-    @Select("select * from tbl_book")
+//    @Select("select * from tbl_book")
     List<Book> queryBookAll();
 }
